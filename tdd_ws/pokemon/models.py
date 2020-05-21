868d9febca26fecdd83e4b1c89a8ffb0bd10cbff
Jésica Collado <collado.jesica@gmail.com>
2020-05-21 12:55:42 -0300

    Add some models

--- tdd_ws/pokemon/models.py
+++ tdd_ws/pokemon/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Move(models.Model):
+    name = models.CharField(max_length=200)
+    power = models.IntegerField()
+
+
+class Trainer(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.CharField(max_length=200)
+
+
+class Team(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+
+class Pokemon(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    specie = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200)
+    level = models.IntegerField()
+    known_moves = models.ManyToManyField(Move)
