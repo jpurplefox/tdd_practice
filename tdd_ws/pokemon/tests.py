5567e6779f687c144726ca160041edf976eb20d5
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 16:53:59 -0300

    Make another test

--- tdd_ws/pokemon/tests.py
+++ tdd_ws/pokemon/tests.py
@@ -4,7 +4,7 @@ from pokemon.models import Trainer, Team, Pokemon, Move
 
 
 class LearningMovesTestCase(APITestCase):
-    def test_a_given_pokemon_can_learn_a_new_move(self):
+    def test_a_pokemon_can_learn_a_new_move(self):
         trainer = Trainer.objects.create(name='red', age=16)
         team = Team.objects.create(name='starting team', trainer=trainer)
         charmander = Pokemon.objects.create(specie='charmander', level=5, team=team)
@@ -18,3 +18,18 @@ class LearningMovesTestCase(APITestCase):
 
         self.assertEqual(charmander.known_moves.count(), 1)
         self.assertIn(ember, charmander.known_moves.all())
+
+    def test_a_pokemon_can_not_learn_a_move_twice(self):
+        trainer = Trainer.objects.create(name='red', age=16)
+        team = Team.objects.create(name='starting team', trainer=trainer)
+        charmander = Pokemon.objects.create(specie='charmander', level=5, team=team)
+
+        ember = Move.objects.create(name='ember', power=40)
+        charmander.known_moves.add(ember)
+
+        url = '/pokemon/{}/moves/'.format(charmander.pk)
+        response = self.client.post(url, {'move_id': ember.id})
+
+        self.assertEqual(response.status_code, 400)
+
+        self.assertEqual(charmander.known_moves.count(), 1)
