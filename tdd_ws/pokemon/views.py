c242dbbfdb39dcb2ba221fbf84c19dde97d49521
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 16:54:21 -0300

    Make it pass

--- tdd_ws/pokemon/views.py
+++ tdd_ws/pokemon/views.py
@@ -8,5 +8,7 @@ class Moves(APIView):
     def post(self, request, pk):
         move = Move.objects.get(id=request.data.get('move_id'))
         pokemon = Pokemon.objects.get(pk=pk)
+        if move in pokemon.known_moves.all():
+            return Response({}, status=400)
         pokemon.known_moves.add(move)
         return Response()
