65f126fda561c1ff47b3f7b9af8be2dfca6cd87f
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 16:54:21 -0300

    Make it pass

--- tdd_ws/pokemon/views.py
+++ tdd_ws/pokemon/views.py
@@ -8,8 +8,7 @@ class Moves(APIView):
     def post(self, request, pk):
         move = Move.objects.get(id=request.data.get('move_id'))
         pokemon = Pokemon.objects.get(pk=pk)
-        try:
-            pokemon.learn(move)
-        except pokemon.CanNotLearnMove as e:
-            return Response({'error': str(e)}, 400)
+        if move in pokemon.known_moves.all():
+            return Response({}, status=400)
+        pokemon.known_moves.add(move)
         return Response()
