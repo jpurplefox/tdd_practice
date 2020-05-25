d7b92ed0c49419cebd036feb149b2339032e12ef
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 17:38:16 -0300

    Add factories

--- tdd_ws/pokemon/tests.py
+++ tdd_ws/pokemon/tests.py
@@ -1,15 +1,13 @@
 from rest_framework.test import APITestCase
 
-from pokemon.models import Trainer, Team, Pokemon, Move
+from pokemon.factories import PokemonFactory, MoveFactory
 
 
 class LearningMovesTestCase(APITestCase):
     def test_a_pokemon_can_learn_a_new_move(self):
-        trainer = Trainer.objects.create(name='red', age=16)
-        team = Team.objects.create(name='starting team', trainer=trainer)
-        charmander = Pokemon.objects.create(specie='charmander', level=5, team=team)
+        charmander = PokemonFactory(specie='charmander')
 
-        ember = Move.objects.create(name='ember', power=40)
+        ember = MoveFactory(name='ember', power=40)
 
         url = '/pokemon/{}/moves/'.format(charmander.pk)
         response = self.client.post(url, {'move_id': ember.id})
@@ -20,11 +18,9 @@ class LearningMovesTestCase(APITestCase):
         self.assertIn(ember, charmander.known_moves.all())
 
     def test_a_pokemon_can_not_learn_a_move_twice(self):
-        trainer = Trainer.objects.create(name='red', age=16)
-        team = Team.objects.create(name='starting team', trainer=trainer)
-        charmander = Pokemon.objects.create(specie='charmander', level=5, team=team)
+        charmander = PokemonFactory(specie='charmander')
 
-        ember = Move.objects.create(name='ember', power=40)
+        ember = MoveFactory(name='ember', power=40)
         charmander.learn(ember)
 
         url = '/pokemon/{}/moves/'.format(charmander.pk)
@@ -37,20 +33,11 @@ class LearningMovesTestCase(APITestCase):
         self.assertEqual(charmander.known_moves.count(), 1)
 
     def test_a_pokemon_can_not_learn_more_than_four_moves(self):
-        trainer = Trainer.objects.create(name='red', age=16)
-        team = Team.objects.create(name='starting team', trainer=trainer)
-        charmander = Pokemon.objects.create(specie='charmander', level=5, team=team)
+        charmander = PokemonFactory(specie='charmander')
+        for move in MoveFactory.create_batch(4):
+            charmander.learn(move)
 
-        scratch = Move.objects.create(name='scratch', power=40)
-        growl = Move.objects.create(name='growl', power=0)
-        ember = Move.objects.create(name='ember', power=40)
-        smokescreen = Move.objects.create(name='smokescreen', power=0)
-        charmander.learn(scratch)
-        charmander.learn(growl)
-        charmander.learn(ember)
-        charmander.learn(smokescreen)
-
-        dragon_breath = Move.objects.create(name='dragon-breath', power=60)
+        dragon_breath = MoveFactory(name='dragon-breath', power=60)
 
         url = '/pokemon/{}/moves/'.format(charmander.pk)
         response = self.client.post(url, {'move_id': dragon_breath.id})
@@ -60,7 +47,4 @@ class LearningMovesTestCase(APITestCase):
         self.assertEqual('charmander can not learn more than four moves', response.data['error'])
 
         self.assertEqual(charmander.known_moves.count(), 4)
-        self.assertIn(scratch, charmander.known_moves.all())
-        self.assertIn(growl, charmander.known_moves.all())
-        self.assertIn(ember, charmander.known_moves.all())
-        self.assertIn(smokescreen, charmander.known_moves.all())
+        self.assertNotIn(dragon_breath, charmander.known_moves.all())
