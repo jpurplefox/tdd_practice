4cd9bd28a37d58c6434d6dee1a1193136bb13de2
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 16:54:21 -0300

    Make it pass

--- tdd_ws/pokemon/views.py
+++ tdd_ws/pokemon/views.py
@@ -9,8 +9,6 @@ class Moves(APIView):
         move = Move.objects.get(id=request.data.get('move_id'))
         pokemon = Pokemon.objects.get(pk=pk)
         if move in pokemon.known_moves.all():
-            return Response({'error': f'{pokemon.specie} already knows {move.name}'}, status=400)
-        if pokemon.known_moves.count() == 4:
-            return Response({'error': f'{pokemon.specie} can not learn more than four moves'}, status=400)
+            return Response({}, status=400)
         pokemon.known_moves.add(move)
         return Response()
