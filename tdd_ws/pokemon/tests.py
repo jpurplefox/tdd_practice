74e64da387d8d5a5e6164ec7fab9d7021d58c23c
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 17:14:21 -0300

    Refactor

--- tdd_ws/pokemon/tests.py
+++ tdd_ws/pokemon/tests.py
@@ -20,7 +20,11 @@ class LearningMovesTestCase(APITestCase):
     def test_a_pokemon_can_not_learn_a_move_twice(self):
         charmander = PokemonFactory(specie='charmander')
 
+<<<<<<< HEAD
         ember = MoveFactory(name='ember', power=40)
+=======
+        ember = Move.objects.create(name='ember', power=40)
+>>>>>>> Refactor
         charmander.learn(ember)
 
         url = '/pokemon/{}/moves/'.format(charmander.pk)
@@ -37,7 +41,20 @@ class LearningMovesTestCase(APITestCase):
         for move in MoveFactory.create_batch(4):
             charmander.learn(move)
 
+<<<<<<< HEAD
         dragon_breath = MoveFactory(name='dragon-breath', power=60)
+=======
+        scratch = Move.objects.create(name='scratch', power=40)
+        growl = Move.objects.create(name='growl', power=0)
+        ember = Move.objects.create(name='ember', power=40)
+        smokescreen = Move.objects.create(name='smokescreen', power=0)
+        charmander.learn(scratch)
+        charmander.learn(growl)
+        charmander.learn(ember)
+        charmander.learn(smokescreen)
+
+        dragon_breath = Move.objects.create(name='dragon-breath', power=60)
+>>>>>>> Refactor
 
         url = '/pokemon/{}/moves/'.format(charmander.pk)
         response = self.client.post(url, {'move_id': dragon_breath.id})
