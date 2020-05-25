3fa2ef74670fc9048009cadac60f2f22c02b7cc4
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 16:47:51 -0300

    Add a test

--- tdd_ws/pokemon/tests.py
+++ tdd_ws/pokemon/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from pokemon.models import Trainer, Team, Pokemon, Move
+
+
+class LearningMovesTestCase(APITestCase):
+    def test_a_given_pokemon_can_learn_a_new_move(self):
+        trainer = Trainer.objects.create(name='red', age=16)
+        team = Team.objects.create(name='starting team', trainer=trainer)
+        charmander = Pokemon.objects.create(specie='charmander', level=5, team=team)
+
+        ember = Move.objects.create(name='ember', power=40)
+
+        url = '/pokemon/{}/moves/'.format(charmander.pk)
+        response = self.client.post(url, {'move_id': ember.id})
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(charmander.known_moves.count(), 1)
+        self.assertIn(ember, charmander.known_moves.all())
