3f032211bb68b66a7e9428ab1a942d6b62f14c24
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 21:04:21 -0300

    Make test pass

--- tdd_ws/pokemon/models.py
+++ tdd_ws/pokemon/models.py
@@ -34,3 +34,8 @@ class Pokemon(models.Model):
         if move in self.known_moves.all():
             raise self.CanNotLearnMove(f'{self.specie} already knows {move.name}')
         self.known_moves.add(move)
+
+    def can_learn(self, move):
+        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{self.specie}/')
+        moves = [move_data['move']['name'] for move_data in response.json()['moves']]
+        return move.name in moves
--- tdd_ws/pokemon/views.py
+++ tdd_ws/pokemon/views.py
@@ -9,7 +9,10 @@ class Moves(APIView):
         move = Move.objects.get(id=request.POST.get('move_id'))
         pokemon = Pokemon.objects.get(pk=pk)
         try:
-            pokemon.learn(move)
+            if pokemon.can_learn(move):
+                pokemon.learn(move)
+            else:
+                return Response({'error': f'{pokemon.specie} can not learn {move.name}'}, 400)
         except pokemon.CanNotLearnMove as e:
             return Response({'error': str(e)}, 400)
         return Response()
