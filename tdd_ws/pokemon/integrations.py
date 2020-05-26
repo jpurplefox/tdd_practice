4d73be8486262b652ddab712e1ba01d2eed13600
Jésica Collado <collado.jesica@gmail.com>
2020-05-25 21:32:30 -0300

    Make the test pass

--- tdd_ws/pokemon/integrations.py
+++ tdd_ws/pokemon/integrations.py
@@ -2,4 +2,7 @@ import requests
 
 
 class MoveLearningChecker:
-    pass
+    def a_specie_can_learn(self, specie, move):
+        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{specie}/')
+        moves = [move_data['move']['name'] for move_data in response.json()['moves']]
+        return move in moves
