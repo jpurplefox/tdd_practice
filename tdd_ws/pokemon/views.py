452e302a1539c808c06178d3731fa215a33f354d
Jésica Collado <collado.jesica@gmail.com>
2020-05-27 15:24:11 -0300

    Replace use of POST

--- tdd_ws/pokemon/views.py
+++ tdd_ws/pokemon/views.py
@@ -6,7 +6,7 @@ from pokemon.models import Pokemon, Move
 
 class Moves(APIView):
     def post(self, request, pk):
-        move = Move.objects.get(id=request.POST.get('move_id'))
+        move = Move.objects.get(id=request.data.get('move_id'))
         pokemon = Pokemon.objects.get(pk=pk)
         pokemon.known_moves.add(move)
         return Response()
